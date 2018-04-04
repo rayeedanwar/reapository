103114055eeb03b6f0b5b5e6d26991f030c2dc7e
Your Name <you@example.com>
2018-04-04 17:14:38 +0100

    java arrays added

---
+++ Java/Arrays/src/Main.java
@@ -0,0 +1,23 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static int[] orderedArray(int[] array) {
+
+        int[] newArray = new int[array.length];
+
+        for (int i = 0; i<array.length; i++) {
+
+
+
+        }
+
+        return newArray;
+
+    }
+
+}
